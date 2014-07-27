c3213e45af81c918c82c575c4f107b4d686fdfa3
Alissa Likavec <alissalikavec@gmail.com>
2014-07-27 22:59:38 +0000

    Set up production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'http://mysterious-waters-8057.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'mysterious-waters-8057.herokuapp.com' }
    config.action_mailer.delivery_method = :smtp
    config.action_mailer.perform_deliveries = true
 end
