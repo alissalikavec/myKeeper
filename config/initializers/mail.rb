ac8bad3af222fdfac99aa847e827f4266cc046e4
Alissa Likavec <alissalikavec@gmail.com>
2014-07-29 03:53:35 +0000

    Mailgun stuff

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,4 +1,3 @@
-if Rails.env.production?
 ActionMailer::Base.smtp_settings = {
   port:              587, 
   address:           'smtp.mailgun.org',
@@ -12,21 +11,3 @@ ActionMailer::Base.delivery_method = :smtp
 
 # Makes debugging *way* easier.
 ActionMailer::Base.raise_delivery_errors = true
-
-# This interceptor just makes sure that local mail 
-# only emails you.
-# http://edgeguides.rubyonrails.org/action_mailer_basics.html#intercepting-emails
-class DevelopmentMailInterceptor
-  def self.delivering_email(message)
-    message.to =  'youremail@website.com'
-    message.cc = nil
-    message.bcc = nil
-  end
-end
-
-# Locally, outgoing mail will be 'intercepted' by the
-# above DevelopmentMailInterceptor before going out
-if Rails.env.development?
-  ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
-end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   	resources :favorites, only: [:create, :destroy]
   end
   root 'bookmarks#index'
+  post :incoming, to: 'incoming#create'
 end
