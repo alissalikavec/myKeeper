43af8fa727558d2eb0f9ae694224fa043b5e98f7
Alissa Likavec <alissalikavec@gmail.com>
2014-08-02 19:57:41 +0000

    Fixing getting user email from email

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -5,9 +5,12 @@ class IncomingController < ApplicationController
 
   def create
     puts "INCOMING PARAMS HERE: #{params}"
-    Bookmark.create!({title: params[:'stripped-text'], tag_list: params[:subject]})
-    User.find_by(email: params[:sender])
+    @user = User.find_by(email: params[:sender])
+    @bookmark = Bookmark.new
+    @bookmark.title = params[:'stripped-text']
+    @bookmark.tag_list = params[:subject]
+    @bookmark.user = @user
+    @bookmark.save
     head 200
-
   end
 end
